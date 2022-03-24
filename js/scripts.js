9fa59c34d69e2187cf122509922dee8a4c9c295e
Marcus <marcanthonyzpadilla@gmail.com>
2022-03-24 16:28:22 -0700

    complete mvp for triangle calc
    Mark McConnell<amark.mcconnell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
+
 $(document).ready(function () {
+  $("#Triangle").submit(function (event) {
+    
+    event.preventDefault();
 
+    const base = parseInt($("#base").val());
+    const height = parseInt($("#height").val());
+    const angle = parseInt($("#angle").val());
+    const output = $("#output");
 
+    if (base === height && base === angle) {
+      output.text("an Equilateral");
+    } else if (base === height && angle > 0) {
+      output.text("an Isosceles");
+    } else if ((base + height) > angle){
+      output.text("a Scalene");
+    } else {
+      output.text("NO")
+    }
+      
+  });
 
-  
 });
